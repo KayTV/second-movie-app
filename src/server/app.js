49edb8a1010dff2e35c35ef9182e7fa1fcf331e2
Kaylyn Van Norstrand <kaylyn.vannorstrand@gmail.com>
2016-03-22 15:50:04 -0600

    new npms

--- src/server/app.js
+++ src/server/app.js
@@ -6,6 +6,9 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var swig = require('swig');
+var session = require('express-session');
+var flash = require('connect-flash');
+var passport = require('./lib/auth');
 
 
 // *** routes *** //
@@ -31,6 +34,14 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({
+  secret: process.env.SECRET_KEY || 'change_me',
+  resave: false,
+  saveUninitialized: true
+}));
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, '../client')));
 
 
